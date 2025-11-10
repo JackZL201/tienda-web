3ad4b291ebe402a8ad9d9d7e209ba5d9c501890d
JackZL201 <dindon2001@hotmail.com>
2025-11-10 03:11:55 -0600

    Delete src/main/java/tienda/models/Descuento.java

--- src/main/java/tienda/models/Descuento.java
+++
@@ -1,9 +0,0 @@
-package tienda.models;
-
-public class Descuento {
-    private final String etiqueta;
-    private final double monto;
-    public Descuento(String etiqueta, double monto) { this.etiqueta = etiqueta; this.monto = monto; }
-    public String getEtiqueta() { return etiqueta; }
-    public double getMonto() { return monto; }
-}
